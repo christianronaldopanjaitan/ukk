3bd795e4852dee8c81deb475e126c73c663f1fe7
christianronaldopanjaitan <ct9354218@gmail.com>
2026-04-08 18:27:17 +0700

    Add files via upload

---
+++ pengaduan_db.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Apr 2026 pada 13.22
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pengaduan_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', '$2y$10$OrCVp1niIoxF/nd1ovNaceZjLsfkQZzf36QHf.pfMm20VX6OLerxW'),
+('admin1', '$2y$10$X8h8XOgtbJv4pst2vT.iV.y9ZRIKpy5.sybSZtOoj4YBo5M7ZAlvK');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `aspirasi`
+--
+
+CREATE TABLE `aspirasi` (
+  `id_aspirasi` int(5) NOT NULL,
+  `status` enum('Menunggu','Proses','Selesai') NOT NULL DEFAULT 'Menunggu',
+  `id_kategori` int(5) DEFAULT NULL,
+  `feedback` text DEFAULT NULL,
+  `tanggal` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `aspirasi`
+--
+
+INSERT INTO `aspirasi` (`id_aspirasi`, `status`, `id_kategori`, `feedback`, `tanggal`) VALUES
+(3, 'Proses', NULL, 'sabar masih di percepat', '2026-02-18 19:56:00'),
+(4, 'Selesai', NULL, 'heheha', '2026-02-19 17:06:34');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `input_aspirasi`
+--
+
+CREATE TABLE `input_aspirasi` (
+  `id_pelaporan` int(5) NOT NULL,
+  `nis` varchar(18) DEFAULT NULL,
+  `id_kategori` int(5) DEFAULT NULL,
+  `lokasi` varchar(50) DEFAULT NULL,
+  `ket` varchar(50) DEFAULT NULL,
+  `tanggal` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `input_aspirasi`
+--
+
+INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `ket`, `tanggal`) VALUES
+(3, '12345678910', 1, 'ruang tu', 'uang pip belum cair kanya', '2026-02-18 19:56:00'),
+(4, '1111111111111', 3, 'asddlasdw', 'niga sss', '2026-02-19 17:06:34'),
+(5, '12345678910', 2, 'kantin', 'paldwsndw', '2026-02-22 09:20:18');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(5) NOT NULL,
+  `ket_kategori` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `ket_kategori`) VALUES
+(1, 'Aspirasi Sekolah'),
+(2, 'Masalah Fasilitas'),
+(3, 'Lain-lain');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nis` varchar(18) NOT NULL,
+  `kelas` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `siswa`
+--
+
+INSERT INTO `siswa` (`nis`, `kelas`) VALUES
+('1111111111111', '11'),
+('12345678910', 'rpl');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indeks untuk tabel `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  ADD PRIMARY KEY (`id_aspirasi`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indeks untuk tabel `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  ADD PRIMARY KEY (`id_pelaporan`),
+  ADD KEY `nis` (`nis`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  MODIFY `id_aspirasi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  MODIFY `id_pelaporan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  ADD CONSTRAINT `aspirasi_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  ADD CONSTRAINT `input_aspirasi_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `input_aspirasi_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
